90fca64f5b299d03fd577b77c77d29f3e94b50ab
adimiuprix <miuprix@gmail.com>
2024-07-27 11:31:48 +0700

    Licensi form up

--- app/Views/licence_check.php
+++ app/Views/licence_check.php
@@ -1,11 +1,29 @@
 <?= $this->extend('partialls/layout') ?>
 <?= $this->section('content') ?>
-    <h1>Check License</h1>
-    <form action="<?= base_url('cekval') ?>" method="post">
-        <label for="domain">Domain:</label><br>
-        <input type="text" id="domain" name="domain"><br>
-        <label for="license_key">License Key:</label><br>
-        <input type="text" id="license_key" name="license_key"><br><br>
-        <button type="submit">Check</button>
-    </form>
+<main>
+    <section class="my-lg-12 my-8">
+        <div class="container">
+            <div class="col-12 col-md-12">
+                <div class="mb-5">
+                    <h1 class="mb-1 h2 fw-bold">Check product license</h1>
+                </div>
+                <form action="<?= base_url('cekval') ?>" method="post" class="needs-validation">
+                    <div class="row g-3">
+                        <div class="col-12">
+                            <label class="form-label visually-hidden">Domain</label>
+                            <input type="text" id="domain" name="domain" class="form-control" placeholder="Domain" required>
+                        </div>
+                        <div class="col-12">
+                            <label class="form-label visually-hidden">License</label>
+                            <input type="text" id="license_key" name="license_key" class="form-control" placeholder="License" required>
+                        </div>
+                        <div class="col-12 d-grid">
+                            <button type="submit" class="btn btn-primary">Check</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</main>
 <?= $this->endSection(); ?>
